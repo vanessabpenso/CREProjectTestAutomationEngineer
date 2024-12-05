a642f75250e2e27448058ebe0dfcc2a42a2fcdf7
vanessabpenso <vanessabpenso@gmail.com>
2024-12-05 17:10:52 +0000

    Update SetupBrowser.java

--- src/test/java/setup/SetupBrowser.java
+++ src/test/java/setup/SetupBrowser.java
@@ -6,37 +6,40 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import java.net.MalformedURLException;
-import java.net.URL;
+
 
 public class SetupBrowser {
-	public static WebDriver driver;
-	
-	public static WebDriver startBrowser(String browser) {
-		switch (browser.toLowerCase()) {
-		case "chrome":
-			 WebDriverManager.chromedriver().setup();
-			 driver = new ChromeDriver();
-			 
-			break;
-		case "firefox":
-			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
-			break;
-		case "edge":
-			WebDriverManager.edgedriver().setup();
-			driver = new EdgeDriver();
-			break;
-		case "safari":
-			driver = new SafariDriver();
-			break;
-		default:
-			throw new IllegalArgumentException("Unsupported browser: " + browser);
-		}
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
-		return driver;
-	}
+    public static WebDriver startBrowser(String browser) {
+        WebDriver webDriver;
+
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                webDriver = new ChromeDriver();
+                break;
+
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                webDriver = new FirefoxDriver();
+                break;
+
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                webDriver = new EdgeDriver();
+                break;
+
+            case "safari":
+                webDriver = new SafariDriver();
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
+
+        driver.set(webDriver); // Assign to ThreadLocal
+        return webDriver;
+    }
 
 }
